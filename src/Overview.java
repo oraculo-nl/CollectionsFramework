91d0f697ffdd775fd92da4710b4d3a8da889a967
oraculo-nl <info@oraculo.nl>
2021-07-27 10:55:35 +0200

    Initial commit

---
+++ src/Overview.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Overview {
+
+    Iterable<String> iterable;
+    Collection<String> collection;
+    List<String> list;
+    Set<String> set;
+    Queue<String> queue;
+    Map<String, String> map;
+
+
+    ArrayList<String> arrayList = new ArrayList<>();
+    LinkedList<String> linkedList = new LinkedList<>();
+    Vector<String> vector = new Vector<>();
+
+    HashSet<String> hashSet = new HashSet<>();
+    LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+    TreeSet<String> treeSet = new TreeSet<>();
+
+    ConcurrentHashMap<String, String> concurrentHashMap;
+
+    Hashtable<String, String> hashtable;
+
+    TreeMap<String,String> treeMap;
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
